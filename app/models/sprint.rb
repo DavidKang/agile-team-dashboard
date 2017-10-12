3dbd1cef57fb7157551ec88475f374a55dc506ca
David Kang <dkang@suse.com>
2017-10-12 17:48:17 +0200

    Add validations and scopes in Sprint
    We added some needed validation, scopes and tests.

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -1,7 +1,10 @@
 class Sprint < ApplicationRecord
   has_many :meetings, dependent: :destroy
+
+  scope :finished, (-> { where('DATE(end_date) < ?', Time.zone.today).order(start_date: :asc) })
+
   validates :number, :start_date, :end_date, presence: true
-  validate :starts_on_weekday, :ends_on_weekday
+  validate :starts_on_weekday, :ends_on_weekday, :ends_before_start, :sprint_collision
 
   after_create :create_meetings
 
@@ -25,11 +28,28 @@ class Sprint < ApplicationRecord
   private
 
   def starts_on_weekday
+    return unless start_date
     errors[:start_date] << 'can not be on weekend' if start_date.try(:on_weekend?)
   end
 
   def ends_on_weekday
+    return unless start_date && end_date
     errors[:end_date] << 'can not be on weekend' if end_date.try(:on_weekend?)
+  end
+
+  def ends_before_start
+    return unless start_date && end_date
+    errors[:end_date] << 'can not end before start' if start_date > end_date
+  end
+
+  def sprint_collision
+    return unless Sprint.current
+    if start_date && (Sprint.current.start_date..Sprint.current.end_date).cover?(start_date)
+      errors[:start_date] << 'cannot start in current sprint days'
+    end
+
+    return unless end_date && (Sprint.current.start_date..Sprint.current.end_date).cover?(end_date)
+    errors[:end_date] << 'cannot end in current sprint days'
   end
 
   def create_meetings
--- spec/models/sprint_spec.rb
+++ spec/models/sprint_spec.rb
@@ -4,4 +4,48 @@ RSpec.describe Sprint, type: :model do
   %i(number start_date end_date).each do |attr|
     it { should validate_presence_of(attr) }
   end
+
+  context 'validate' do
+    context 'start_on_weekend' do
+      let(:sprint) { build(:sprint, start_date: '2017-10-07', end_date: '2017-10-12') }
+
+      before do
+        sprint.valid?
+      end
+
+      it { expect(sprint.errors.full_messages).to eq(['Start date can not be on weekend']) }
+    end
+
+    context 'end_on_weekend' do
+      let(:sprint) { build(:sprint, start_date: '2017-10-06', end_date: '2017-10-08') }
+
+      before do
+        sprint.valid?
+      end
+
+      it { expect(sprint.errors.full_messages).to eq(['End date can not be on weekend']) }
+    end
+
+    context 'ends_before_start' do
+      let(:sprint) { build(:sprint, start_date: '2017-10-06', end_date: '2017-10-02') }
+
+      before do
+        sprint.valid?
+      end
+
+      it { expect(sprint.errors.full_messages).to eq(['End date can not end before start']) }
+    end
+
+    context 'ends_before_start' do
+      let!(:sprint) { create(:sprint, start_date: '2017-10-02', end_date: '2017-10-13') }
+      let(:invalid_sprint) { build(:sprint, start_date: '2017-10-06', end_date: '2017-10-12') }
+
+      before do
+        invalid_sprint.valid?
+      end
+
+      it { expect(invalid_sprint.errors.full_messages.first).to eq('Start date cannot start in current sprint days') }
+      it { expect(invalid_sprint.errors.full_messages.last).to eq('End date cannot end in current sprint days') }
+    end
+  end
 end
